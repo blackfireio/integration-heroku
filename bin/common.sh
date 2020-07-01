e93b79163ddb6e5c782c99752aab5ef53523ccad
Jérôme Vieilledent <lolautruche@gmail.com>
2020-07-01 17:28:30 +0200

    Move base functions to common.sh

---
+++ bin/common.sh
@@ -0,0 +1,16 @@
+function error() {
+  echo " !     $*" >&2
+  exit 1
+}
+
+function topic() {
+  echo "-----> $*"
+}
+
+function indent() {
+  c='s/^/       /'
+  case $(uname) in
+    Darwin) sed -l "$c";;
+    *)      sed -u "$c";;
+  esac
+}
